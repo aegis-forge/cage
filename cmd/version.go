f467686a1b3473607f3b3aaa93ce049f0afd146a
edoriggio <edo.riggio19@gmail.com>
2025-08-18 14:46:43 +0200

    added structs for building semvers and semver ranges

--- cmd/version.go
+++ cmd/version.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"regexp"
+	"slices"
 	"strings"
 
 	"golang.org/x/mod/semver"
@@ -37,36 +39,98 @@ func (s Semver) IsValid() bool {
 	return semver.IsValid(string(s))
 }
 
-// ================
-// ==== Semver ====
-// ================
+// Equals checks if two [Semver] are equal
+func (s Semver) Equals(sv Semver) bool {
+	return semver.Compare(string(s), string(sv)) == 0
+}
+
+// ======================
+// ==== VersionRange ====
+// ======================
+
+// operators is a slice of all the supported operators 
+var operators = []string{ ">", ">=", "<", "<=", "=", "=="}
 
 // VersionRange is used to store ranges of versions. If `start` is empty, the
 // range will be `< end`. If `end` is empty, then it means `== start`.
 type VersionRange struct {
-	start Semver
-	end   Semver
+	start        Semver
+	end          Semver
+	includeRight bool
+	includeLeft  bool
 }
 
 // NewVersionRange creates a new [VersionRanges] and checks if it's valid
-func NewVersionRange(start Semver, end Semver) (*VersionRange, error) {
+func NewVersionRange(start, end Semver, left, right bool) (*VersionRange, error) {
 	if start == "" && end == "" {
 		return nil, errors.New("at least one semver needs not to be empty")
 	}
-	
+
 	if (start == "" && !end.IsValid()) || (!start.IsValid() && end == "") {
 		return nil, errors.New("if one semver is empty, the other needs to be valid")
 	}
-	
+
 	if start != "" && end != "" {
 		if !start.IsValid() || !end.IsValid() {
 			return nil, errors.New("if not empty, both semvers need to be valid")
 		}
-		
-		if semver.Compare(string(start), string(end)) != -1 {
-			return nil, errors.New("start is greater or equal to end")
+
+		if semver.Compare(string(start), string(end)) == 1 {
+			return nil, errors.New("start is greater to end")
 		}
 	}
 
-	return &VersionRange{start: start, end: end}, nil
+	return &VersionRange{
+		start: start, end: end, includeLeft: left, includeRight: right,
+	}, nil
+}
+
+// NewVersionRangeString creates a range given a range string (e.g. `>= v1.0`)
+func NewVersionRangeString(stringRange string) (*VersionRange, error) {
+	if stringRange == "" {
+		return nil, errors.New("stringRange cannot be null")
+	}
+
+	regex := regexp.MustCompile(`^([<>]=?|==?)?\s*([0-9A-z.+\-]+)$`)
+	matches := regex.FindStringSubmatch(stringRange)
+
+	if len(matches) != 3 {
+		return nil, errors.New("the passed string does not contain a range")
+	}
+
+	operator := matches[1]
+	version, err := NewSemver(matches[2])
+
+	if err != nil {
+		return nil, err
+	}
+	
+	if !slices.Contains(operators, operator) {
+		return nil, fmt.Errorf("the operator \"%s\" is not valid", operator)
+	}
+
+	var finalRange *VersionRange
+
+	switch operator {
+	case "=", "==":
+		finalRange, _ = NewVersionRange(version, version, true, true)
+	case ">=":
+		finalRange, _ = NewVersionRange(version, "", true, false)
+	case ">":
+		finalRange, _ = NewVersionRange(version, "", false, false)
+	case "<=":
+		v0, _ := NewSemver("0.0.0")
+		finalRange, _ = NewVersionRange(v0, version, false, true)
+	case "<":
+		v0, _ := NewSemver("0.0.0")
+		finalRange, _ = NewVersionRange(v0, version, false, false)
+	}
+
+	return finalRange, nil
+}
+
+// Equals checks if two [VersionRange] are equal (i.e. same start and end)
+func (v *VersionRange) Equals(vr VersionRange) bool {
+	return v.start.Equals(vr.start) && v.end.Equals(vr.end) && 
+		v.includeLeft == vr.includeLeft && v.includeRight == vr.includeRight
 }
--- cmd/version_test.go
+++ cmd/version_test.go
@@ -1,6 +1,9 @@
 package cmd
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // ===================
 // ==== NewSemver ====
@@ -49,8 +52,8 @@ func TestNewSemverInvalid(t *testing.T) {
 func TestNewVersionRangeValid(t *testing.T) {
 	start, _ := NewSemver("v1")
 	end, _ := NewSemver("v2")
-	
-	if _, err := NewVersionRange(start, end); err != nil {
+
+	if _, err := NewVersionRange(start, end, false, false); err != nil {
 		t.Error(err)
 	}
 }
@@ -58,8 +61,8 @@ func TestNewVersionRangeValid(t *testing.T) {
 func TestNewVersionRangeInvalid(t *testing.T) {
 	start := Semver("master")
 	end, _ := NewSemver("v2")
-	
-	if _, err := NewVersionRange(start, end); err == nil {
+
+	if _, err := NewVersionRange(start, end, false, false); err == nil {
 		t.Error(err)
 	}
 }
@@ -67,8 +70,8 @@ func TestNewVersionRangeInvalid(t *testing.T) {
 func TestNewVersionRangeValidRange(t *testing.T) {
 	start, _ := NewSemver("v1")
 	end, _ := NewSemver("v2")
-	
-	if _, err := NewVersionRange(start, end); err != nil {
+
+	if _, err := NewVersionRange(start, end, false, false); err != nil {
 		t.Error(err)
 	}
 }
@@ -76,30 +79,237 @@ func TestNewVersionRangeValidRange(t *testing.T) {
 func TestNewVersionRangeInvalidRange(t *testing.T) {
 	start, _ := NewSemver("v2")
 	end, _ := NewSemver("v1")
-	
-	if _, err := NewVersionRange(start, end); err == nil {
+
+	if _, err := NewVersionRange(start, end, false, false); err == nil {
 		t.Error(err)
 	}
 }
 
 func TestNewVersionRangeEmptyStart(t *testing.T) {
 	end, _ := NewSemver("v1")
-	
-	if _, err := NewVersionRange("", end); err != nil {
+
+	if _, err := NewVersionRange("", end, false, false); err != nil {
 		t.Error(err)
 	}
 }
 
 func TestNewVersionRangeEmptyEnd(t *testing.T) {
 	start, _ := NewSemver("v1")
-	
-	if _, err := NewVersionRange(start, ""); err != nil {
+
+	if _, err := NewVersionRange(start, "", false, false); err != nil {
 		t.Error(err)
 	}
 }
 
 func TestNewVersionRangeEmpty(t *testing.T) {
-	if _, err := NewVersionRange("", ""); err == nil {
+	if _, err := NewVersionRange("", "", false, false); err == nil {
 		t.Error(err)
+	}
+}
+
+// ===============================
+// ==== NewVersionRangeString ====
+// ===============================
+
+func TestVersionRangeStringValidLess(t *testing.T) {
+	testRange, _ := NewVersionRange("v0.0.0", "v1.0", false, false)
+
+	if vRange, err := NewVersionRangeString("< 1.0"); err == nil {
+		if !testRange.Equals(*vRange) {
+			t.Error("the two ranges are different")
+		}
+	} else {
+		t.Error(err.Error())
+	}
+}
+
+func TestVersionRangeStringValidGreater(t *testing.T) {
+	testRange, _ := NewVersionRange("v1.0.0", "", false, false)
+
+	if vRange, err := NewVersionRangeString("> 1.0.0"); err == nil {
+		if !testRange.Equals(*vRange) {
+			t.Error("the two ranges are different")
+		}
+	} else {
+		t.Error(err.Error())
+	}
+}
+
+func TestVersionRangeStringValidLessEqual(t *testing.T) {
+	testRange, _ := NewVersionRange("v0.0.0", "v2", false, true)
+
+	if vRange, err := NewVersionRangeString("<= v2"); err == nil {
+		if !testRange.Equals(*vRange) {
+			t.Error("the two ranges are different")
+		}
+	} else {
+		t.Error(err.Error())
+	}
+}
+
+func TestVersionRangeStringValidGreaterEqual(t *testing.T) {
+	testRange, _ := NewVersionRange("v1.0.0+ciao-test", "", true, false)
+
+	if vRange, err := NewVersionRangeString(">=1.0.0+ciao-test"); err == nil {
+		if !testRange.Equals(*vRange) {
+			t.Error("the two ranges are different")
+		}
+	} else {
+		t.Error(err.Error())
+	}
+}
+
+func TestVersionRangeStringValidEqual(t *testing.T) {
+	testRange, _ := NewVersionRange("v11.0.2", "v11.0.2", true, true)
+
+	if vRange, err := NewVersionRangeString("==v11.0.2"); err == nil {
+		if !testRange.Equals(*vRange) {
+			t.Error("the two ranges are different")
+		}
+	} else {
+		t.Error(err.Error())
+	}
+}
+
+func TestVersionRangeStringValidEqualAlt(t *testing.T) {
+	testRange, _ := NewVersionRange("v11.0.2", "v11.0.2", true, true)
+
+	if vRange, err := NewVersionRangeString("= v11.0.2"); err == nil {
+		fmt.Println(testRange)
+		fmt.Println(vRange)
+
+		if !testRange.Equals(*vRange) {
+			t.Error("the two ranges are different")
+		}
+	} else {
+		t.Error(err.Error())
+	}
+}
+
+func TestVersionRangeStringInvalid(t *testing.T) {
+	if _, err := NewVersionRangeString(">==1.0"); err == nil {
+		t.Error("range should not be valid")
+	}
+
+	if _, err := NewVersionRangeString("===v1.0"); err == nil {
+		t.Error("range should not be valid")
+	}
+
+	if _, err := NewVersionRangeString("<==v1.0"); err == nil {
+		t.Error("range should not be valid")
+	}
+}
+
+func TestVersionRangeStringInvalidVersion(t *testing.T) {
+	if _, err := NewVersionRangeString(">=ciao"); err == nil {
+		t.Error("range should not be valid")
+	}
+}
+
+func TestVersionRangeStringInvalidEmpty(t *testing.T) {
+	if _, err := NewVersionRangeString(""); err == nil {
+		t.Error("range should not be valid")
+	}
+}
+
+// =============================
+// ==== VersionRange Equals ====
+// =============================
+
+func TestVersionRangeEqualsTrue(t *testing.T) {
+	v1, _ := NewSemver("v1")
+	v2, _ := NewSemver("v2")
+
+	r1, _ := NewVersionRange(v1, v2, false, false)
+	r2, _ := NewVersionRange(v1, v2, false, false)
+
+	if !r1.Equals(*r2) {
+		t.Error("ranges are not the same")
+	}
+}
+
+func TestVersionRangeEqualsEmptyStart(t *testing.T) {
+	v1, _ := NewSemver("v1")
+
+	r1, _ := NewVersionRange("", v1, false, false)
+	r2, _ := NewVersionRange("", v1, false, false)
+
+	if !r1.Equals(*r2) {
+		t.Error("ranges are not the same")
+	}
+}
+
+func TestVersionRangeEqualsEmptyEnd(t *testing.T) {
+	v1, _ := NewSemver("v1")
+
+	r1, _ := NewVersionRange(v1, "", false, false)
+	r2, _ := NewVersionRange(v1, "", false, false)
+
+	if !r1.Equals(*r2) {
+		t.Error("ranges are not the same")
+	}
+}
+
+func TestVersionRangeEqualsDifferentEnd(t *testing.T) {
+	v1, _ := NewSemver("v1")
+	v2, _ := NewSemver("v2")
+	v3, _ := NewSemver("v3")
+
+	r1, _ := NewVersionRange(v1, v2, false, false)
+	r2, _ := NewVersionRange(v1, v3, false, false)
+
+	if r1.Equals(*r2) {
+		t.Error("ranges are not supposed to be the same")
+	}
+}
+
+func TestVersionRangeEqualsDifferentStart(t *testing.T) {
+	v1, _ := NewSemver("v1")
+	v2, _ := NewSemver("v2")
+	v3, _ := NewSemver("v3")
+
+	r1, _ := NewVersionRange(v1, v3, false, false)
+	r2, _ := NewVersionRange(v2, v3, false, false)
+
+	if r1.Equals(*r2) {
+		t.Error("ranges are not supposed to be the same")
+	}
+}
+
+func TestVersionRangeEqualsDifferent(t *testing.T) {
+	v1, _ := NewSemver("v1")
+	v2, _ := NewSemver("v2")
+	v3, _ := NewSemver("v3")
+	v4, _ := NewSemver("v4")
+
+	r1, _ := NewVersionRange(v1, v2, false, false)
+	r2, _ := NewVersionRange(v3, v4, false, false)
+
+	if r1.Equals(*r2) {
+		t.Error("ranges are not supposed to be the same")
+	}
+}
+
+func TestVersionRangeEqualsDifferentIncludesLeft(t *testing.T) {
+	v1, _ := NewSemver("v1")
+	v2, _ := NewSemver("v2")
+
+	r1, _ := NewVersionRange(v1, v2, true, false)
+	r2, _ := NewVersionRange(v1, v2, false, false)
+
+	if r1.Equals(*r2) {
+		t.Error("ranges are not supposed to be the same")
+	}
+}
+
+func TestVersionRangeEqualsDifferentIncludesRight(t *testing.T) {
+	v1, _ := NewSemver("v1")
+	v2, _ := NewSemver("v2")
+
+	r1, _ := NewVersionRange(v1, v2, false, true)
+	r2, _ := NewVersionRange(v1, v2, false, false)
+
+	if r1.Equals(*r2) {
+		t.Error("ranges are not supposed to be the same")
 	}
 }
