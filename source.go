af27258013c49e9881e0f63d04193c0c8467b6f4
edoriggio <edo.riggio19@gmail.com>
2025-08-21 00:40:04 +0200

    added source struct + tests

---
+++ source.go
@@ -0,0 +1,275 @@
+package cage
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"slices"
+	"strings"
+)
+
+// ================
+// ==== Source ====
+// ================
+
+// Source is an interface that describes the source of vulnerabilities
+type Source interface {
+	GetVulnerabilities(Package) ([]Vulnerability, error)
+	CompareVulnerabilities([]Vulnerability, Package) ([]Vulnerability, error)
+}
+
+// ================
+// ==== Github ====
+// ================
+
+// Github is a source of vulnerabilities (https://github.com/advisories)
+type Github struct {
+	token string
+}
+
+// SetToken sets the GitHub token and checks if its valid
+func (g *Github) SetToken(token string) error {
+	if token == "" {
+		return errors.New("token must not be an empty string")
+	}
+
+	url := "https://api.github.com"
+	bearer := "Bearer " + token
+
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+	req.Header.Add("Authorization", bearer)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		return errors.New("an error happened when trying to verify the token")
+	}
+
+	switch res.StatusCode {
+	case http.StatusOK:
+		g.token = token
+		return nil
+	case http.StatusUnauthorized:
+		return errors.New("the given token is not valid")
+	default:
+		return fmt.Errorf(
+			"when verifying the token, a %d error code was returned",
+			res.StatusCode,
+		)
+	}
+}
+
+// GetVulnerabilities retrieves all vulnerabilities for the given package version
+func (g Github) GetVulnerabilities(packg Package) ([]Vulnerability, error) {
+	url := fmt.Sprintf(
+		"https://api.github.com/repos/%s/%s/security-advisories",
+		packg.vendor, packg.product,
+	)
+
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+
+	if g.token != "" {
+		req.Header.Add("Authorization", "Bearer "+g.token)
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	type identifier struct {
+		Ghsa string `json:"ghsa_id"`
+	}
+
+	var identifiers []identifier
+
+	if err := json.NewDecoder(res.Body).Decode(&identifiers); err != nil {
+		return nil, err
+	}
+
+	// ======
+
+	type rawResponse struct {
+		Cve             string `json:"cve_id"`
+		Severity        string `json:"severity"`
+		Published       string `json:"published_at"`
+		Withdrawn       string `json:"withdrawn_at"`
+		Vulnerabilities []struct {
+			Package struct {
+				Ecosystem string `json:"ecosystem"`
+				Name      string `json:"name"`
+			} `json:"package"`
+			VulnRange    string `json:"vulnerable_version_range"`
+			PatchedRange string `json:"first_patched_version"`
+		} `json:"vulnerabilities"`
+		Cvss struct {
+			Score float32 `json:"score"`
+		} `json:"cvss"`
+		Cwes []struct {
+			Id string `json:"cwe_id"`
+		} `json:"cwes"`
+	}
+
+	var rawVulns []rawResponse
+	var vulns []Vulnerability
+
+	for _, id := range identifiers {
+		url = fmt.Sprintf("https://api.github.com/advisories/%s", id.Ghsa)
+		req, _ = http.NewRequest(http.MethodGet, url, nil)
+
+		if g.token != "" {
+			req.Header.Add("Authorization", "Bearer "+g.token)
+		}
+
+		res, err = client.Do(req)
+
+		if err != nil {
+			return nil, err
+		}
+
+		if res.StatusCode == http.StatusNotFound {
+			continue
+		}
+
+		defer res.Body.Close()
+
+		var rawVuln rawResponse
+
+		if err := json.NewDecoder(res.Body).Decode(&rawVuln); err != nil {
+			return nil, err
+		}
+
+		rawVulns = append(rawVulns, rawVuln)
+	}
+
+	for _, rawVuln := range rawVulns {
+		var vulnerableRanges []VersionRange
+		var patchedRanges []VersionRange
+
+		for _, vuln := range rawVuln.Vulnerabilities {
+			packgRaw := vuln.Package
+			name := fmt.Sprintf("%s/%s", packg.vendor, packg.product)
+
+			valid := rawVuln.Withdrawn != ""
+			valid = packgRaw.Ecosystem == "actions" && packgRaw.Name == name
+
+			if !valid {
+				continue
+			}
+
+			versions := strings.Split(vuln.VulnRange, ", ")
+
+			if len(versions) == 2 {
+				r1, err := NewVersionRangeString(versions[0])
+				r2, err := NewVersionRangeString(versions[1])
+
+				if err != nil {
+					return nil, err
+				}
+
+				r, _ := NewVersionRange(r1.start, r2.end, r1.includeLeft, r2.includeRight)
+				vulnerableRanges = append(vulnerableRanges, *r)
+			} else {
+				for _, vulnString := range versions {
+					r, err := NewVersionRangeString(vulnString)
+
+					if err != nil {
+						return nil, err
+					}
+
+					vulnerableRanges = append(vulnerableRanges, *r)
+				}
+			}
+
+			if vuln.PatchedRange != "" {
+				v, err := NewSemver(vuln.PatchedRange)
+
+				if err != nil {
+					return nil, err
+				}
+
+				r, err := NewVersionRange(v, "", true, false)
+
+				if err != nil {
+					return nil, err
+				}
+				
+				patchedRanges = append(patchedRanges, *r)
+			} else {
+				patchedRanges = append(patchedRanges, VersionRange{})
+			}
+		}
+
+		var cwes []string
+
+		for _, cwe := range rawVuln.Cwes {
+			cwes = append(cwes, cwe.Id)
+		}
+
+		vuln, err := NewVulnerability(
+			rawVuln.Cve, cwes, rawVuln.Cvss.Score, rawVuln.Published,
+			vulnerableRanges, patchedRanges, "2006-01-02T15:04:05Z",
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		vulns = append(vulns, *vuln)
+	}
+	
+	fmt.Println(rawVulns)
+	fmt.Println()
+
+	return vulns, err
+}
+
+// CompareVulnerabilities checks if the [Package] version is contained in the
+// vulns slice.
+func (g Github) CompareVulnerabilities(vulns []Vulnerability, packg Package) ([]Vulnerability, error) {
+	var vulnerabilitiesRaw []Vulnerability
+	var vulnerabilitiesClean []Vulnerability
+
+	for _, vuln := range vulns {
+		if packg.published.Before(vuln.published) {
+			continue
+		}
+
+		vulnerabilitiesRaw = append(vulnerabilitiesRaw, vuln)
+	}
+
+	for _, vuln := range vulnerabilitiesRaw {
+		var vs []bool
+
+		for i, vulnRange := range vuln.rangesVulnerable {
+			v := false
+			vp := vuln.rangesPatched[i]
+			
+			if vulnRange.Contains(packg.version) {
+				v = true
+			}
+			
+			if vp.start == "" && vp.end == "" {
+				continue
+			} else if vp.Contains(packg.version) {
+				v = false
+			}
+			
+			vs = append(vs, v)
+		}
+		
+		comp := slices.Compact(vs)
+
+		if len(comp) > 1 || (len(comp) == 1 && comp[0]) {
+			vulnerabilitiesClean = append(vulnerabilitiesClean, vuln)
+		}
+	}
+
+	return vulnerabilitiesClean, nil
+}
---
+++ source_test.go
@@ -0,0 +1,212 @@
+package cage
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func getToken() string {
+	if err := godotenv.Load(); err != nil {
+		panic(".env not found or syntax error in the .env file")
+	}
+
+	if os.Getenv("GITHUB_TOKEN") == "" {
+		panic("cannot find GITHUB_TOKEN variable")
+	}
+
+	return os.Getenv("GITHUB_TOKEN")
+}
+
+// =========================
+// ==== Github SetToken ====
+// =========================
+
+func TestSetTokenValid(t *testing.T) {
+	if os.Getenv("TEST_ENV") == "workflow" {
+		return
+	}
+
+	gh := &Github{}
+
+	if err := gh.SetToken(getToken()); err != nil {
+		t.Error(err)
+	}
+
+	if gh.token == "" {
+		t.Error("token was not set")
+	}
+}
+
+func TestSetTokenEmpty(t *testing.T) {
+	gh := &Github{}
+
+	if err := gh.SetToken(""); err == nil {
+		t.Error(err)
+	}
+}
+
+func TestSetTokenUnauthorized(t *testing.T) {
+	gh := &Github{}
+
+	if err := gh.SetToken("a"); err.Error() != "the given token is not valid" {
+		t.Error(err)
+	}
+}
+
+// =======================================
+// ==== Github CompareVulnerabilities ====
+// =======================================
+
+func TestPackageIsVulnerableTrueA(t *testing.T) {
+	s := Github{}
+
+	v1, _ := NewSemver("2.294.0")
+	v2, _ := NewSemver("2.296.1")
+	v3, _ := NewSemver("0.0.0")
+	v4, _ := NewSemver("2.283.4")
+
+	vr1, _ := NewVersionRange(v1, v2, true, false)
+	vr2, _ := NewVersionRange(v3, v4, false, false)
+	pr1, _ := NewVersionRangeString("2.296.2")
+	pr2, _ := NewVersionRangeString("2.293.1")
+
+	vua, _ := NewVulnerability(
+		"a", []string{}, 1.0, "2025-07-25T17:51:51Z", []VersionRange{*vr1},
+		[]VersionRange{*pr1}, "2006-01-02T15:04:05Z",
+	)
+
+	vub, _ := NewVulnerability(
+		"b", []string{}, 1.0, "2025-07-25T17:51:51Z", []VersionRange{*vr2},
+		[]VersionRange{*pr2}, "2006-01-02T15:04:05Z",
+	)
+
+	v, _ := NewSemver("2.296.0")
+	p, _ := NewPackage("a", "b", time.Now(), v)
+	vs, _ := s.CompareVulnerabilities([]Vulnerability{*vua, *vub}, *p)
+
+	if len(vs) != 1 || vs[0].cve != "a" {
+		t.Error("vulnerability \"a\" should have been returned")
+	}
+}
+
+func TestPackageIsVulnerableTrueB(t *testing.T) {
+	s := Github{}
+
+	v1, _ := NewSemver("2.294.0")
+	v2, _ := NewSemver("2.296.1")
+	v3, _ := NewSemver("0.0.0")
+	v4, _ := NewSemver("2.283.4")
+
+	vr1, _ := NewVersionRange(v1, v2, true, false)
+	vr2, _ := NewVersionRange(v3, v4, false, false)
+	pr1, _ := NewVersionRangeString("2.296.2")
+	pr2, _ := NewVersionRangeString("2.293.1")
+
+	vua, _ := NewVulnerability(
+		"a", []string{}, 1.0, "2025-07-25T17:51:51Z", []VersionRange{*vr1},
+		[]VersionRange{*pr1}, "2006-01-02T15:04:05Z",
+	)
+
+	vub, _ := NewVulnerability(
+		"b", []string{}, 1.0, "2025-07-25T17:51:51Z", []VersionRange{*vr2},
+		[]VersionRange{*pr2}, "2006-01-02T15:04:05Z",
+	)
+
+	v, _ := NewSemver("2.283.3")
+	p, _ := NewPackage("a", "b", time.Now(), v)
+	vs, _ := s.CompareVulnerabilities([]Vulnerability{*vua, *vub}, *p)
+
+	if len(vs) != 1 || vs[0].cve != "b" {
+		t.Error("vulnerability \"b\" should have been returned")
+	}
+}
+
+func TestPackageIsVulnerableTrueAll(t *testing.T) {
+	s := Github{}
+
+	v1, _ := NewSemver("2.294.0")
+	v2, _ := NewSemver("2.296.1")
+	v3, _ := NewSemver("0.0.0")
+	v4, _ := NewSemver("2.295.0")
+
+	vr1, _ := NewVersionRange(v1, v2, true, false)
+	vr2, _ := NewVersionRange(v3, v4, false, false)
+	pr1, _ := NewVersionRangeString("2.296.2")
+	pr2, _ := NewVersionRangeString("2.293.1")
+
+	vua, _ := NewVulnerability(
+		"a", []string{}, 1.0, "2025-07-25T17:51:51Z", []VersionRange{*vr1},
+		[]VersionRange{*pr1}, "2006-01-02T15:04:05Z",
+	)
+
+	vub, _ := NewVulnerability(
+		"b", []string{}, 1.0, "2025-07-25T17:51:51Z", []VersionRange{*vr2},
+		[]VersionRange{*pr2}, "2006-01-02T15:04:05Z",
+	)
+
+	v, _ := NewSemver("2.294.2")
+	p, _ := NewPackage("a", "b", time.Now(), v)
+	vs, _ := s.CompareVulnerabilities([]Vulnerability{*vua, *vub}, *p)
+
+	if len(vs) != 2 || (vs[0].cve != "a" && vs[0].cve != "b") {
+		t.Error("vulnerabilities \"a\" and \"b\" should have been returned")
+	}
+}
+
+func TestPackageIsVulnerableFalse(t *testing.T) {
+	s := Github{}
+
+	v1, _ := NewSemver("2.294.0")
+	v2, _ := NewSemver("2.296.1")
+	v3, _ := NewSemver("0.0.0")
+	v4, _ := NewSemver("2.283.4")
+
+	vr1, _ := NewVersionRange(v1, v2, true, false)
+	vr2, _ := NewVersionRange(v3, v4, false, false)
+	pr1, _ := NewVersionRangeString("2.296.2")
+	pr2, _ := NewVersionRangeString("2.293.1")
+
+	vua, _ := NewVulnerability(
+		"a", []string{}, 1.0, "2025-07-25T17:51:51Z", []VersionRange{*vr1},
+		[]VersionRange{*pr1}, "2006-01-02T15:04:05Z",
+	)
+
+	vub, _ := NewVulnerability(
+		"b", []string{}, 1.0, "2025-07-25T17:51:51Z", []VersionRange{*vr2},
+		[]VersionRange{*pr2}, "2006-01-02T15:04:05Z",
+	)
+
+	v, _ := NewSemver("2.296.2")
+	p, _ := NewPackage("a", "b", time.Now(), v)
+	vs, _ := s.CompareVulnerabilities([]Vulnerability{*vua, *vub}, *p)
+
+	if len(vs) != 0 {
+		t.Error("no vulnerability should have been returned")
+	}
+}
+
+func TestPackageIsVulnerableFalseFuture(t *testing.T) {
+	s := Github{}
+
+	v1, _ := NewSemver("0.0.0")
+	v2, _ := NewSemver("2.294.0")
+
+	vr1, _ := NewVersionRange(v1, v2, true, false)
+	pr1, _ := NewVersionRangeString("2.296.2")
+
+	vua, _ := NewVulnerability(
+		"a", []string{}, 1.0, time.Now().Add(time.Hour).Format("2006-01-02T15:04:05Z"),
+		[]VersionRange{*vr1}, []VersionRange{*pr1}, "2006-01-02T15:04:05Z",
+	)
+
+	v, _ := NewSemver("2")
+	p, _ := NewPackage("a", "b", time.Now(), v)
+	vs, _ := s.CompareVulnerabilities([]Vulnerability{*vua}, *p)
+
+	if len(vs) != 0 {
+		t.Error("no vulnerability should have been returned")
+	}
+}
